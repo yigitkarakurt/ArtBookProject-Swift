4eceb79a28f28b6df0191754815793be1c96642f
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-10-06 17:49:09 +0300

    designed

---
+++ ArtBookProject/DetailsVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsVC.swift
+//  ArtBookProject
+//
+//  Created by Yiğit Karakurt on 6.10.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+   
+
+}
